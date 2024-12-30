019ebdad0848e322caca2b8b26e64968deed1ac7
Abel Chacko Mathew <achusep19@gmail.com>
2024-12-29 23:26:30 -0600

    Initial Commit

---
+++ src/test/java/com/campuspathfinder/pathfinder_app/PathfinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuspathfinder.pathfinder_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
